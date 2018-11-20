165b82676fc902d37681736c95fe0e1da6cfc6d3
Alexey Kadyrov <akadyrov@oneviewhealthcare.com>
2018-11-20 09:29:24 +0000

    added PostgreSQL tests

---
+++ PostgreSQL.IntegrationTests/TestEnvironmentSetup.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using DocaLabs.HybridPortBridge.ClientAgent.Config;
+using DocaLabs.HybridPortBridge.Config;
+using DocaLabs.HybridPortBridge.ServiceAgent.Config;
+using DocaLabs.Qa;
+using NUnit.Framework;
+
+namespace PostgreSQL.IntegrationTests
+{
+    [SetUpFixture]
+    public class TestEnvironmentSetup
+    {
+        public const string EntityPath = "ovc-cicd-test-sql";
+
+        private const string ClientAgentName = "PostgeSQL.ClientAgent";
+        private const string ServiceAgentName = "PostgeSQL.MySqlServiceAgent";
+
+        private static readonly string[] ServiceAgentArgs = QaDefaults
+            .GetSerilogConfigurationArgs(ServiceAgentName, QaDefaults.MakeDefaultLogPath(ServiceAgentName))
+            .MergeRange(new
+            {
+                AgentMetrics = new AgentMetricsOptions
+                {
+                    MetricsOptions =
+                    {
+                        DefaultContextLabel = "Azure Relay Port Bridge",
+                        GlobalTags =
+                        {
+                            { "agent", "Service" }
+                        },
+                        Enabled = true,
+                        ReportingEnabled = true
+                    },
+                    ReportingOptions =
+                    {
+                        ReportingFlushIntervalSeconds = 30,
+                        ReportFile = Path.Combine(AppContext.BaseDirectory, "metrics-service.txt")
+                    }
+                }
+            }.ToConfigurationArgs())
+            .MergeRange(new
+            {
+                PortBridge = new ServiceAgentOptions
+                {
+                    EntityPaths =
+                    {
+                        EntityPath
+                    }
+                }
+            }
+            .ToConfigurationArgs());
+
+        private static readonly string[] ClientAgentArgs = QaDefaults
+            .GetSerilogConfigurationArgs(ClientAgentName, QaDefaults.MakeDefaultLogPath(ClientAgentName))
+            .MergeRange(new
+            {
+                AgentMetrics = new AgentMetricsOptions
+                {
+                    MetricsOptions =
+                    {
+                        DefaultContextLabel = "Azure Relay Port Bridge",
+                        GlobalTags =
+                        {
+                            { "agent", "Client" }
+                        },
+                        Enabled = true,
+                        ReportingEnabled = true
+                    },
+                    ReportingOptions =
+                    {
+                        ReportingFlushIntervalSeconds = 30,
+                        ReportFile = Path.Combine(AppContext.BaseDirectory, "metrics-client.txt")
+                    }
+                }
+            }.ToConfigurationArgs())
+            .MergeRange(new
+            {
+                PortBridge = new ClientAgentOptions
+                {
+                    PortMappings =
+                    {
+                        {
+                            "5040", new PortMappingOptions
+                            {
+                                EntityPath = EntityPath,
+                                RemoteTcpPort = 5432,
+                                AcceptFromIpAddresses =
+                                {
+                                    "127.0.0.1"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .ToConfigurationArgs());
+
+
+        private Thread _serviceAgentThread;
+        private Thread _clientAgentThread;
+
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+            Console.WriteLine("Setting up test environment...");
+
+            StartServiceAgent();
+
+            StartClientAgent();
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            Console.WriteLine("Cleaning up test environment...");
+
+            StopClientAgent();
+
+            StopServiceAgent();
+        }
+
+        private void StartServiceAgent()
+        {
+            _serviceAgentThread = new Thread(() => DocaLabs.HybridPortBridge.ServiceAgent.Console.Program.Main(ServiceAgentArgs))
+            {
+                IsBackground = true
+            };
+
+            _serviceAgentThread.Start();
+        }
+
+        private void StartClientAgent()
+        {
+            _clientAgentThread = new Thread(() => DocaLabs.HybridPortBridge.ClientAgent.Console.Program.Main(ClientAgentArgs))
+            {
+                IsBackground = true
+            };
+
+            _clientAgentThread.Start();
+        }
+
+        private void StopServiceAgent()
+        {
+            try
+            {
+                DocaLabs.HybridPortBridge.ServiceAgent.Console.Program.Blocker.Release();
+
+                if (_serviceAgentThread == null)
+                    return;
+
+                if (!_serviceAgentThread.Join(TimeSpan.FromSeconds(5)))
+                    _serviceAgentThread.Abort();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void StopClientAgent()
+        {
+            try
+            {
+                DocaLabs.HybridPortBridge.ClientAgent.Console.Program.Blocker.Release();
+
+                if (_clientAgentThread == null)
+                    return;
+
+                if (!_clientAgentThread.Join(TimeSpan.FromSeconds(5)))
+                    _clientAgentThread.Abort();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ PostgreSQL.IntegrationTests/Tests/PortBridgeTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Npgsql;
+using NUnit.Framework;
+
+namespace PostgreSQL.IntegrationTests.Tests
+{
+    [TestFixture]
+    public class PortBridgeTests
+    {
+        private const string ConnectionString = "Host=localhost;port=5040;username=postgres;Password=password;Database=postgres";
+        private const string CreateTableClause = "CREATE TABLE Records(Id int NOT NULL, RecordValue int NOT NULL)";
+
+        private const string DropTableClause = "DROP TABLE Records";
+
+
+        [Test]
+        public async Task Should_execute_queries_against_mysql_behind_port_bridge()
+        {
+            using (var connection = new NpgsqlConnection(ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+
+                    await CreateTable(connection);
+
+                    await AddRecord(connection, 1, 11);
+                    await AddRecord(connection, 2, 12);
+                    await AddRecord(connection, 3, 13);
+                    await AddRecord(connection, 4, 14);
+                    await AddRecord(connection, 5, 15);
+
+                    var command = new NpgsqlCommand("SELECT Id, RecordValue FROM Records ORDER BY Id", connection);
+
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        var id = 0;
+
+                        while (await reader.ReadAsync())
+                        {
+                            id++;
+
+                            reader["Id"].Should().Be(id);
+                            reader["RecordValue"].Should().Be(10 + id);
+                        }
+
+                        id.Should().Be(5);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+                finally
+                {
+                    await DropTable(connection);
+                }
+            }
+        }
+
+        private static async Task CreateTable(NpgsqlConnection connection)
+        {
+            using (var command = new NpgsqlCommand(CreateTableClause, connection))
+            {
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        private static async Task DropTable(NpgsqlConnection connection)
+        {
+            try
+            {
+                using (var command = new NpgsqlCommand(DropTableClause, connection))
+                {
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+            catch
+            {
+                // intentional
+            }
+        }
+
+        private static async Task AddRecord(NpgsqlConnection connection, int id, int recordValue)
+        {
+            using (var command = new NpgsqlCommand("INSERT INTO Records (Id, RecordValue) VALUES(@Id, @RecordValue)", connection))
+            {
+                command.Parameters.AddWithValue("@Id", id);
+                command.Parameters.AddWithValue("@RecordValue", recordValue);
+
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
